66514f2d96d306a6f361fe10cfb787bf22b650be
Birgith Jóhansdóttir Christiansen <birgithchristiansen@hotmail.com>
2020-12-09 10:16:02 +0100

    Frontend

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const nodemon = require("nodemon");
+const app = express();
+const port = 8080;
+app.use(express.static("frontend"));
+
+//____________________________________________________________________________
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/frontend/index.html');
+});
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/frontend/resetPassword.html');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/frontend/index.html')
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port:", port)
+});
+
