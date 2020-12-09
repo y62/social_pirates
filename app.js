d3d3fd6118432226fcad650bc17c0b297e1e322a
HansenKenneth <arme@taxastork.dk>
2020-12-09 10:50:17 +0100

    making frontend work with app.js

--- app.js
+++ app.js
@@ -10,15 +10,26 @@ app.get('/', (req, res) => {
     return res.sendFile(__dirname + '/frontend/index.html');
 });
 
-app.get('/', (req, res) => {
+app.get('/resetPassword', (req, res) => {
     return res.sendFile(__dirname + '/frontend/resetPassword.html');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/frontend/index.html')
-})
-
-app.listen(port, () => {
-    console.log("Server is running on port:", port)
+app.get('/information', (req, res) => {
+    return res.sendFile(__dirname + '/frontend/information.html');
 });
 
+app.get('/createMember', (req, res) => {
+    return res.sendFile(__dirname + '/frontend/createMember.html');
+});
+
+app.get('/contact', (req, res) => {
+    return res.sendFile(__dirname + '/frontend/contact.html');
+});
+
+app.get('/adminHome', (req, res) => {
+    return res.sendFile(__dirname + '/frontend/adminHome.html');
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port: ", port)
+});
