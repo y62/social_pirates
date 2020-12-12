ff6e6ceb332b812f8d43501abf4a59fdff29dd5c
y62 <y62@outlook.dk>
2020-12-12 23:21:05 +0100

    login finished

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-
+const path = require('path');
+const session = require('express-session');
 const express = require("express");
 const nodemon = require("nodemon");
 const database = require("./database");
--- routes.js
+++ routes.js
@@ -1,11 +1,51 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const path = require('path');
+const session = require('express-session');
+const database = require('./database');
+
 const app = express();
 const port = 8080;
 
 app.use(express.static("frontend"));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.set('views',path.join(__dirname,'frontend/views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+
+app.post('/auth', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (email && password) {
+        connection.query('SELECT * FROM members WHERE email = ? AND password = ?', [email, password], (error, results, fields) => {
+            if (results.length > 0) {
+                req.session.loggedin = true;
+                req.session.email = email;
+                res.redirect('/home');
+            } else {
+                res.send('Forkert Email eller Adgangskode');
+            }
+            res.end();
+        });
+
+    } else {
+        res.send('Indtast Email / Adgangskode');
+        res.end();
+    }
+});
 
 app.get('/', (req, res) => {
-    return res.sendFile(__dirname + '/frontend/index.html');
+    req.session.loggedin = false;
+    res.sendFile(path.join(__dirname + '/frontend/index.html'));
 });
 
 app.get('/resetPassword', (req, res) => {
@@ -18,7 +58,7 @@ app.get('/information', (req, res) => {
 
 app.get('/resetPassword', (req, res) => {
     res.sendFile(__dirname + '/frontend/index.html')
-})
+});
 
 app.get('/createMember', (req, res) => {
     return res.sendFile(__dirname + '/frontend/createMember.html');
@@ -31,6 +71,14 @@ app.get('/contact', (req, res) => {
 app.get('/adminHome', (req, res) => {
     return res.sendFile(__dirname + '/frontend/adminHome.html');
 });
+
+app.get('/home', (req, res) => {
+    if (req.session.loggedin) {
+        return res.sendFile(__dirname + '/frontend/home.html');
+    } else {
+        res.send('Husk at logge ind!');
+    }
+    });
 
 app.listen(port, () => {
     console.log("Server is running on port: ", port)
